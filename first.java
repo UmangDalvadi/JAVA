afe3a79b2c6a75295296c650cbdfaf63bcd60305
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-16 02:45:52 +0530

    add

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 public class first {
     public static void main(String args[]) {
-            System.out.print("Umang Dalvadi !!");
+        System.out.print("Umang Dalvadi !!");
     }
 
 }
---
+++ sum.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args){
+        Scanner u= new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int a=u.nextInt();
+        System.out.print("Enter 2st number: ");
+        int b=u.nextInt();
+
+        System.out.println("Sum is: "+ (a+b));
+
+
+
+    }
+}
