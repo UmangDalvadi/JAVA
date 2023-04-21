88a5743d6c96dd7a4f17f37bc8d921536d0a0ee8
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-22 04:11:51 +0530

    add

---
+++ methods.java
@@ -0,0 +1,19 @@
+public class methods {
+
+    static int logic(int a,int b){
+        return a+b;
+    }
+    int logic1(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=6;
+
+        System.out.println(logic(a,b));
+
+        methods lc=new methods();
+        System.out.println(lc.logic1(a,b));
+    }
+}
