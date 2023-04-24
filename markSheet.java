98deaa3441f310adefcfcaceb92be0747516e8d6
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-24 14:49:53 +0530

    add

--- markSheet.java
+++ markSheet.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class markSheet
 {
     public static void main(String[] args){
-        Scanner u=new Scanner(System.in);
-        System.out.println("Enter OS marks : ");
-        float a= u.nextFloat();
-        System.out.println("Enter COA marks : ");
-        float b= u.nextFloat();
-        System.out.println("Enter DBMS marks : ");
-        float c= u.nextFloat();
-        System.out.println("Enter DSA marks : ");
-        float d= u.nextFloat();
-        System.out.println("Enter OOPs marks : ");
-        float e= u.nextFloat();
+        try (Scanner u = new Scanner(System.in)) {
+            System.out.println("Enter OS marks : ");
+            float a= u.nextFloat();
+            System.out.println("Enter COA marks : ");
+            float b= u.nextFloat();
+            System.out.println("Enter DBMS marks : ");
+            float c= u.nextFloat();
+            System.out.println("Enter DSA marks : ");
+            float d= u.nextFloat();
+            System.out.println("Enter OOPs marks : ");
+            float e= u.nextFloat();
 
-        System.out.println("Total marks : "+(a+b+c+d+e));
-        System.out.println("Percentage : "+(a+b+c+d+e)/5);
+            System.out.println("Total marks : "+(a+b+c+d+e));
+            System.out.println("Percentage : "+(a+b+c+d+e)/5);
+        }
     }
 }
