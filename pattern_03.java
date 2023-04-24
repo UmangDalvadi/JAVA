1fd5152d639ccd1d565847db810e228dfdbf2fba
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-24 23:46:35 +0530

    add

---
+++ pattern_03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pattern_03 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        int n=sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <i; k++) {
+                
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
