a2995f10456fc5417607e3a0ac5d3c973aec821b
UmangDalvadi <umangdalvadi8@gmail.com>
2023-05-07 23:56:33 +0530

    add

--- DSA-in-JAVA/String/palindrom.java
+++ DSA-in-JAVA/String/palindrom.java
@@ -8,17 +8,17 @@ public class palindrom {
 
     static boolean pld(String str) {
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
-                return true;
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your string : ");
+        System.out.print("Enter your string : ");
         String str = sc.nextLine();
 
         System.out.println(pld(str));
