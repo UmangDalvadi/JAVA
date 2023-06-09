05a3a37a942cd5f29f49ee7ca8258188bd27ab9e
UmangDalvadi <umangdalvadi8@gmail.com>
2023-06-09 09:58:59 +0530

    add

---
+++ DSA-in-JAVA/Bit_Manipulation/getithbit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class getithbit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter index for Ith bit : ");
+        int n2 = sc.nextInt();
+
+        int bitmask = (1<<n2);
+
+        if ((n1 & bitmask) == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);
+
+        }
+    }
+
+}
