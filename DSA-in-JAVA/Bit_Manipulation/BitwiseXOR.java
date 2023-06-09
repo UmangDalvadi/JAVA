04b9d0678fac6dffa1d342e6c9ba85056c8f6cf9
UmangDalvadi <umangdalvadi8@gmail.com>
2023-06-09 09:15:52 +0530

    add

---
+++ DSA-in-JAVA/Bit_Manipulation/BitwiseXOR.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BitwiseXOR {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter Second number : ");
+        int n2 = sc.nextInt();
+
+        System.out.printf("Ans : " + (n1 ^ n2));
+    }
+}
