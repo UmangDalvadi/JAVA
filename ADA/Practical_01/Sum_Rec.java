c88f86d3e8910418cce191d370aa49502dee0f81
UmangDalvadi <umangdalvadi8@gmail.com>
2023-08-26 21:10:25 +0530

    add

--- ADA/Practical_01/Sum_Rec.java
+++ ADA/Practical_01/Sum_Rec.java
@@ -3,33 +3,18 @@ import java.util.Scanner;
 public class Sum_Rec{
 
     static int sum(int n){
-
-        // int s=0;
-        
-        
         if (n==1) {
             return 1;
         }
-        
         else{
-            // s+=n;
-            // System.out.print(s);
             return n+sum(n-1);
         }
-        
-        
+         
     }
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {   
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
-
-        // sum(n);
-
         System.out.println("Sum: "+sum(n));
-
-
     }
 }
